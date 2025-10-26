3b81be8198d9c373a84b74e58030812ca92934b5
Will Albeck <will@albeck.me>
2025-10-26 03:18:45 -0400

    Implement Earthquake model and confirm all tests passing

--- server/models.py
+++ server/models.py
@@ -1,8 +1,18 @@
+# server/models.py
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
 metadata = MetaData()
-
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+class Earthquake(db.Model):
+    __tablename__ = "earthquakes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    magnitude = db.Column(db.Float)
+    location = db.Column(db.String)
+    year = db.Column(db.Integer)
+
+    def __repr__(self):
+        """Readable string representation of an Earthquake record."""
+        return f"<Earthquake {self.id}, {self.magnitude}, {self.location}, {self.year}>"
